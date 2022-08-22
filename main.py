aa3bf86c6e4bb43fa04f0bf2310cc045844a3b53
Artificialheaven <69500511+Artificialheaven@users.noreply.github.com>
2022-08-22 13:27:06 +0800

    主要配置

---
+++ main.py
@@ -0,0 +1,37 @@
+import asyncio
+import configparser
+
+import Bot
+import Listener
+
+
+def receivegroupmsg(group_id, user_id, msg):
+    """收到群消息，group_id为群号，user_id为发送人，msg为消息内容"""
+    Listener.recG(group_id, user_id, msg, Bot.sendGroupMsg)
+    return
+
+
+def receiveprivatemsg(user_id, msg):
+    """收到私聊消息，user_id为发送人，msg为消息内容"""
+    Listener.recP(user_id, msg, Bot.sendPrivateMsg)
+    return
+
+
+Plugins_name = 'Plugin'
+#   插件名
+Plugins_info = ''
+config = configparser.ConfigParser()
+config.read('./config.ini')
+ip = config['Bot']['ip']
+hp = config['Bot']['hp']
+wp = config['Bot']['wp']
+Bot = Bot.Bot(ip, hp, wp)
+print(f'即将启动 {Plugins_name} 服务，目标位于http://%s:%s/ & ws://%s:%s' % (ip, hp, ip, wp))
+if Plugins_info != '':
+    print(f'{Plugins_info}')
+print('高性能しぶそうしん機 !!')
+
+#   初始化一个事件监听器
+Listener = Listener.Listener(ip, hp, wp)
+
+asyncio.run(Bot.run(receivegroupmsg, receiveprivatemsg))
